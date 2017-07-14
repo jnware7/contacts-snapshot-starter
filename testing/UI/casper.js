f13e8ebe73e8695792818e430ab83e687e9dae64
jnware7 <jnware7@gmail.com>
2017-07-14 13:53:39 -0700

    added integration tests

--- testing/UI/casper.js
+++ testing/UI/casper.js
@@ -18,6 +18,17 @@ casper.test.begin('Testing the page status', function(test){
     test.assertTextDoesntExist('Jared','Jared has been removed')
     test.assertTextExists('Tanner','Tanner has not been removed')
   })
+
+  casper.thenEvaluate(function(term){
+    document.querySelector('input[name="q"]').setAttribute('value', term)
+    document.querySelector('input[type="submit"]').submit()
+  },  {term:"Tanner"})
+
+  casper.then(function(){
+    test.assertTextDoesntExist('Jeff','Jeff is not found')
+    test.assertTextExists('Tanner','Tanner is found')
+  })
+
   casper.run(function(){
     test.done()
   })
@@ -26,3 +37,24 @@ casper.test.begin('Testing the page status', function(test){
 //goes to contacts page
 //clicking delete button
 //testing for removed element
+
+casper.test.begin('Testing the page status', function(test){
+  casper.start(url,function(){
+    test.assertHttpStatus(200,'Page is up and running')
+  })
+
+  casper.thenEvaluate(function(){
+    document.querySelector('.delete-contact').click('.delete-contact')
+  })
+  casper.setFilter('page.confirm',function(popup){
+    console.log("popup==>",popup)
+    return popup === "Are you sure you want to delete this contact?" ? true : false
+  })
+  casper.then(function(){
+    console.log(casper.getPageContent())
+    test.assertTextDoesntExist('Jared','Jared is deleted')
+  })
+  casper.run(function(){
+    test.done()
+  })
+})
